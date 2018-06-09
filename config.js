6c7a52c9dcb70d6d9976033ecb1ed9807cd060d6
Vehicle Tech. Lab 안홍범 <hb.ahn@sk.com>
2018-06-09 17:03:56 +0900

    change smart fleet api address

--- config.js
+++ config.js
@@ -2,7 +2,7 @@
 
 module.exports = {
   platform : {
-    baseurl : "https://smartfleet.sktelecom.com:8080/api"
+    baseurl : "https://smartfleet.sktelecom.com:9901/api"
   },
   appkey : "",
   port : 3000
